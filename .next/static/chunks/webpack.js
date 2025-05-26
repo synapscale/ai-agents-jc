f52c2c3012e5cdaf07dca508e489f5806e07bd74
synapscale <contato@synapscale.com.br>
2025-05-26 14:52:00 +0000

    11

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -190,7 +190,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("d3e9fa0c277e2515")
+/******/ 		__webpack_require__.h = () => ("876f913c67a5fe3e")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
